fe39768356731c48a5fbc6cedb751be730f82c3e
Anastasiia <ambarnova94@gmail.com>
2025-09-13 22:30:17 +0200

    delete cache

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oZLRpCUs5cKrar1UzkCE0/Bv2MVxp9Owc5vgDFuSX3o=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"IRhroXSCuwT1He4izd6BaDi0dkBay1LZcjuY9V3MGuw=\"\n}"
